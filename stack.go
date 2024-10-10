0da9edd683cde7354d090813718d1f541ffb2788
MuriloBarros304 <murilobarros304@gmail.com>
2024-10-10 14:31:05 -0300

    stack

---
+++ stack.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "fmt"
+    "errors"
+)
+
+type Stack interface {
+    Size() int
+    Push(e int)
+    Pop() (int, error)
+    Top() (int, error)
+}
+
+type Node struct {
+    v int
+    next *Node
+}
+
+type LinkedStack struct {
+    top *Node
+    inserted int
+}
+
+func (s *LinkedStack) Size() int {
+    return s.inserted
+}
+
+func (s *LinkedStack) Push(e int) {
+    newNode := &Node{v:e}
+    if s.top != nil {
+        newNode.next = s.top
+    }
+    s.top = newNode
+    s.inserted++
+}
+
+func (s *LinkedStack) Pop() (int, error) {
+    if s.top != nil {
+        s.inserted--
+        val := s.top.v
+        s.top = s.top.next
+        return val, nil
+    } else {
+        return 0, errors.New("Empty Stack")
+    }
+}
+
+func (s *LinkedStack) Top() (int, error) {
+    if s.top != nil {
+        return s.top.v, nil
+    } else {
+        return 0, errors.New("Empty Stack")
+    }
+}
+
+func main(){
+    s := &LinkedStack{}
+    s.Push(1)
+    s.Push(4)
+    s.Push(8)
+    s.Push(12)    
+    fmt.Println(s.Top())
+}
